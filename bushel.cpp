9d2ddd3a4235ca368c90b35684dcb92fe190165e
prismika <brantmv@gmail.com>
2019-10-01 18:20:03 -0500

    Add prompt and quit command

--- bushel.cpp
+++ bushel.cpp
@@ -14,10 +14,21 @@ using namespace std;
 
 /*The shell's environment, where variables are stored.*/
 Environment env;
+string prompt = "bushel$ ";
 
 /*Main method. Bootstrap for the entire program.*/
 int main(int argc, char *argv[]){
+	//Setup
 	//env.put_variable("shell","path/to/shell/bushel");
-	
+	//Main prompt loop
+	while(true){
+		cout << prompt;
+		string userInput;
+		getline(cin, userInput);
+		if(!userInput.compare("quit")){
+			exit(0);
+		}
+	}
+
 	return 0;
 }
