bf126f992f10e95458ac6f9993bb3d3fd209e584
prismika <brantmv@gmail.com>
2019-10-03 20:27:39 -0500

    Show PWD in prompt

--- bushel.cpp
+++ bushel.cpp
@@ -12,23 +12,62 @@ Brantley Vose
 
 using namespace std;
 
-/*The shell's environment, where variables are stored.*/
-Environment env;
-string prompt = "bushel$ ";
+string pwd = "";
 
 /*Main method. Bootstrap for the entire program.*/
 int main(int argc, char *argv[]){
 	//Setup
-	//env.put_variable("shell","path/to/shell/bushel");
+	//put in environment: "shell","path/to/shell/bushel"
 	//Main prompt loop
 	while(true){
+		pwd = getenv("PWD");
+		string prompt = "Bushel:" + pwd + "$ ";
 		cout << prompt;
 		string userInput;
 		getline(cin, userInput);
 		if(!userInput.compare("quit")){
 			exit(0);
 		}
+		//parser.parse(&userInput);
+		//while(parser.has_next_command()){
+			//Command *current_command;
+			//current_command = parser.next_command();
+			//execute(current_command);
+		//}
 	}
 
 	return 0;
 }
+
+/*int execute(Command *command){
+	int ret;
+	if(ret = execute_special(command)){
+		return ret;
+	}
+	string command_name = command->get_name();
+	string path = get_env_or_whatever("PATH");
+	//Split at semicolons into vector<string> paths
+	for(int i = 0; i < paths.size(); i++){
+		//Look in paths[i] for a command matching command_name
+		if(!fork()){
+			//put in environment parent
+			execlp("paths[i]" + command_name, command_name, NULL);
+			//This but with arguments that work
+			exit(0);
+		}else{//parent
+			if(!command.background()){
+				wait(NULL);
+			}
+		}
+	}
+	return 0;
+}*/
+
+/*
+int execute_special(Command *command){
+	string command_name = command->name();
+	if(command_name.compare("cd")){
+		...
+	}...
+}
+*/
